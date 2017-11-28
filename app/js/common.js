c0149a615484960f92c4b091ae908c48468c29c2
Gulin Sergey <sg@ya.ru>
2017-11-28 22:41:00 +0300

    com

---
+++ app/js/common.js
@@ -0,0 +1,32 @@
+$(function() {
+
+
+	window.onscroll = function() {
+		scrollFunction();
+		scrollFunctionMenu()};
+
+	function scrollFunction() {
+	    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+	        document.getElementById("myBtn").style.display = "block";
+	    } else {
+	        document.getElementById("myBtn").style.display = "none";
+	    }
+	};
+
+	function scrollFunctionMenu() {
+		var ht = document.getElementById("head").offsetHeight;
+	    if (document.body.scrollTop > ht || document.documentElement.scrollTop > ht) {
+	        document.getElementById("menu").style.position = "fixed";
+	    }
+	    else {
+	    	document.getElementById("menu").style.position = "relative";
+	    }
+	};
+
+});
+
+function topFunction() {
+	    document.body.scrollTop = 0;
+	    document.documentElement.scrollTop = 0;
+	    document.getElementById("myBtn").style.display = "none";
+	};
